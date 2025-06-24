911a835a2f58b5dc93a6ec7a59473273197f01a1
Yovan <yovanjcd@gmail.com>
2025-06-24 16:04:23 +0200

    asd

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import HeroSection from './components/HeroSection';
+import GallerySection from './components/GallerySection';
+import MeasuresSection from './components/MeasuresSection';
+import SocialSection from './components/SocialSection';
+import ContactSection from './components/ContactSection';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <HeroSection />
+      <GallerySection />
+      <MeasuresSection />
+      <SocialSection />
+      <ContactSection />
+    </>
+  );
+}
+
+export default App;
